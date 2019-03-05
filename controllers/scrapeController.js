4cb4cc95eecd8c4744b2cb09b231f4c2ff726159
ASH Walls <admin@arethawalls.com>
2019-03-05 12:37:24 -0700

    Cleaned summary string a bit

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -1,17 +1,19 @@
 const cheerio = require('cheerio'),
       axios = require('axios');
 
-
 module.exports.getArticles = function (req, res) {
     axios.get('https://tucsonfoodie.com/feed/')
     .then(response => {
         let $ = cheerio.load(response.data, {xmlMode: true});
         let articleData = [];
         $('item').each((i, element) => {
+            summaryData = $(element).children('description').text();
+            cleanSummary =  $(summaryData).first('p').text();
+            cleanSummary = cleanSummary.replace(/ \[…\]/, '…');
             articleData.push({
                 url: $(element).children('link').text(),
                 headline: $(element).children('title').text(),
-                summary: $(element).children('description').text()
+                summary: cleanSummary
             })
         })
         res.render('index', {articles: articleData});
