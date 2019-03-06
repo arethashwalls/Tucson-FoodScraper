2e99b9ce2f0063d90038efc153da0506dab69979
ASH Walls <admin@arethawalls.com>
2019-03-06 10:32:18 -0700

    Added toggle to veiw button

--- public/app.js
+++ public/app.js
@@ -1,4 +1,22 @@
-Array.from(document.getElementsByClassName('comment-button'))
-.forEach(commentButton => {
-    const articleId = this.dataset.articleid;
+const forEachOfClass = (className, cb) => {
+    Array.from(document.getElementsByClassName(className))
+    .forEach(element => cb(element));
+}
+
+forEachOfClass('view-button', function(button) {
+    const articleId = button.dataset.articleid;
+    const notesSection = document.querySelector(`#notes-${articleId}`);
+    if(notesSection) {
+        button.addEventListener('click', () => notesSection.classList.toggle('hidden'));
+    } else {
+        button.setAttribute('disabled', true);
+    }
 })
+
+// Array.from(document.getElementsByClassName('comment-button'))
+// .forEach(commentButton => commentButton.addEventListener('click', function() {
+//     const articleId = this.dataset.articleid;
+//     const notesSection = document.querySelector(`#notes-${articleId}`);
+//     if (notesSection) notesSection.classList.toggle('hidden');
+// }));
+
